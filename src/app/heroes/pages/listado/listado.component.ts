1d53255527c5fa14e039b9af5e41ec6506506ba6
Alex Martinez <alex_martinez_3355@hotmail.com>
2021-03-02 10:02:53 -0600

    Heroes Service - Traer información de los héroes.

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-listado',
@@ -8,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  constructor( private heroesS: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesS.getHeroes().subscribe( resp => console.log(resp));
   }
 
 }
